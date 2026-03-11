d784222626b7c9407acc27673b7f075353890cb8
JGuadalupeD <150277290+JGuadalupeD@users.noreply.github.com>
2026-03-11 04:22:50 -0500

    Shorten line length to pass check

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -125,11 +125,7 @@ public class TodoController implements Controller {
   private Bson constructSortingOrder(Context ctx) {
     if (ctx.queryParamMap().containsKey(SORT_BY_KEY)) {
       String sortBy = ctx.queryParam(SORT_BY_KEY);
-      if (
-      sortBy.equals(OWNER_KEY) ||
-      sortBy.equals(STATUS_KEY) ||
-      sortBy.equals(BODY_KEY) ||
-      sortBy.equals(CATEGORY_KEY)) {
+      if (sortBy.equals(OWNER_KEY)||sortBy.equals(STATUS_KEY)||sortBy.equals(BODY_KEY)||sortBy.equals(CATEGORY_KEY)) {
         return Sorts.ascending(sortBy);
       } else {
         throw new BadRequestResponse("Sort by parameter must be 'owner', 'status', 'body', or 'category'.");
