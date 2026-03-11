034e83b1927c6073636756a535a830a9f52e52ad
J Guadalupe Dimayuga <150277290+JGuadalupeD@users.noreply.github.com>
2026-03-11 00:18:35 -0500

    Uncomment assertThrows import in TodoControllerSpec

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -4,7 +4,7 @@ package umm3601.todo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 // import static org.junit.jupiter.api.Assertions.assertNotEquals;
 // import static org.junit.jupiter.api.Assertions.assertNotNull;
-// import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 // import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 // import static org.mockito.ArgumentMatchers.argThat;
